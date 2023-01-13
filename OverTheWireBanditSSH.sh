1f5b515bbb47e6033ff2c83d843ff3caeeb7034d
mohammedajani <119796893+mohammedajani@users.noreply.github.com>
2023-01-13 21:37:18 +0530

    Update OverTheWireBanditSSH.sh

--- OverTheWireBanditSSH.sh
+++ OverTheWireBanditSSH.sh
@@ -5,4 +5,5 @@ echo 'Enter bandil level='
 read a
 
 p=2220
+#ssh may take a bit time to load and we will not see seems like script not working LOL!!!
 ssh bandit$a@bandit.labs.overthewire.org -p $p
